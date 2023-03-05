3c34fdea858fde681b8dd1a23a20b9e9d31570f4
Grayson Miller <grayson.miller124@gmail.com>
2023-03-04 22:52:56 -0700

    Added resizing.

--- fragrunner/fragrunner/__main__.py
+++ fragrunner/fragrunner/__main__.py
@@ -11,15 +11,24 @@ logger = logging.getLogger(__name__)
 
 class FragRunner(arcade.Window):
     def __init__(self, width, height, title):
-        super().__init__(width, height, title)        
-        arcade.set_background_color(arcade.color.AMAZON)
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.BANGLADESH_GREEN)
+        self.shader_root = Path(__file__).parent / "shaders"
+
 
         self.start_time = time.time()
         self.iMouse = [0, 0, 0, 0]
 
         self.quad_fs = arcade.gl.geometry.quad_2d_fs()
         self.passes = {}
-            
+
+    def setup(self, shader_root: Path):
+        self.shader_root = shader_root
+        
+        self.__build_passes(self.shader_root)
+
+        return self
+         
     def on_draw(self):
         arcade.start_render()
 
@@ -43,7 +52,15 @@ class FragRunner(arcade.Window):
         self.ctx.copy_framebuffer(self.passes['image']['fbo'], self.ctx.screen)
     
     def on_mouse_motion(self, x, y, dx, dy):
-        self.iMouse = [x, y, dx, dy]
+        self.iMouse = [x, y, self.iMouse[2], self.iMouse[3]]
+    
+    def on_mouse_press(self, x, y, button, modifiers):
+        self.iMouse = [x, y, button, modifiers]
+
+    def on_resize(self, width, height):
+        super().on_resize(width, height)
+
+        self.__build_passes(self.shader_root)
 
     def __update_uniforms(self, shader):
         try:
@@ -66,13 +83,13 @@ class FragRunner(arcade.Window):
 
         return shader
     
-    def setup(self, shader_root: Path):
+    def __build_passes(self, shader_root):
         # Default vertex shader, just pass through the vertex data.
         vertex_shader = Path(__file__).parent / "default_vert.glsl"
 
         # the main image pass is always named "image", it's technically a frag pass.
         image_shader = shader_root / "image.glsl"
-        passes = parse_image_passes(image_shader)
+        passes = FragRunner.__parse_image_passes(image_shader)
         logger.debug(f"Passes: {list(passes.keys())} {len(passes)}\n{[(k, v['textures']) for k, v in passes.items()]}")
 
         # Create the main program - each pass should be a new program with textures bound appropriately on draw.
@@ -82,106 +99,97 @@ class FragRunner(arcade.Window):
                 fragment_shader=v['source'],
             )
 
-            # self.channels is a list of the names of texture uniforms the pass uses.
-            # These texture uniforms are the render targets of the previous pass by the same name.
-
-            # self.passes is a list of real shader `Program`s for each pass. We prob need to create a texture object for each.
             self.passes[k] = {'prog': program,
                               'fbo': self.ctx.framebuffer(color_attachments=[self.ctx.texture((self.width, self.height))]),
                               'channels': list(v['textures'].values())}
 
-            logger.debug(f"Created pass {k} with channels {self.passes[k]}")
+            logger.debug(f"Created pass {k} with channels {self.passes[k]['channels']} and program {program}")
 
         # Set the default uniforms
         for p in self.passes.values():
             self.__add_default_uniforms(p['prog'])
 
-        # init framebuffer
+    @staticmethod
+    def __parse_image_passes(shader: Path, passes=None, visited=None) -> dict:
+        """ Parse a shader file and return a dictionary of passes. 
+        
+        Each pass is a dictionary with the following keys:
+            textures: A dictionary of iChannelN -> path
+            source: The source code of the shader
 
-        return self
+        A pass is a fragment shader which uses a predefined vertex shader to render a quad.
+        Each pass uses a series of iChannels to pass textures to the shader for rendering.
 
+        The first pass is always the main image pass, and is named "image". The rest of the passes
+        are generated from the iChannels in the main image pass
+        """
+        if passes is None:
+            passes = {}
+        if visited is None:
+            visited = set()
 
-def parse_image_passes(shader: Path, passes=None, visited=None) -> dict:
-    """ Parse a shader file and return a dictionary of passes. 
-    
-    Each pass is a dictionary with the following keys:
-        textures: A dictionary of iChannelN -> path
-        source: The source code of the shader
+        if shader.stem not in passes:
+            passes[shader.stem] = {
+                'textures': {},
+                'source': None,
+            }
 
-    A pass is a fragment shader which uses a predefined vertex shader to render a quad.
-    Each pass uses a series of iChannels to pass textures to the shader for rendering.
+        valid_shader = "#version 430\n\n"
+        # default uniforms
+        valid_shader += "uniform float iTime;\n"
+        valid_shader += "uniform vec2 iResolution;\n"
+        valid_shader += "uniform int iFrame;\n"
+        valid_shader += "uniform vec4 iMouse;\n\n"
 
-    The first pass is always the main image pass, and is named "image". The rest of the passes
-    are generated from the iChannels in the main image pass
-    """
-    if passes is None:
-        passes = {}
-    if visited is None:
-        visited = set()
+        # parse source pass
+        for line in shader.read_text().splitlines():
+            if "fragCoord" in line:
+                line = line.replace("fragCoord", "gl_FragCoord")
 
-    if shader.stem not in passes:
-        passes[shader.stem] = {
-            'textures': {},
-            'source': None,
-        }
+            if line.lstrip().startswith("#iChannel"):
+                # Parse out iChannels for texture uniforms binding.
+                logger.debug(f"Found iChannel in {shader.stem}: {line}")
 
-    valid_shader = "#version 430\n\n"
-    # default uniforms
-    valid_shader += "uniform float iTime;\n"
-    valid_shader += "uniform vec2 iResolution;\n"
-    valid_shader += "uniform int iFrame;\n"
-    valid_shader += "uniform vec4 iMouse;\n\n"
+                channel, path = line.split(" ", 1)
+                path = path.strip('"')
+                if path.startswith("file://"):
+                    if path[7:] not in visited:
+                        visited.add(path[7:])
+                        FragRunner.__parse_image_passes(shader.parent / path[7:], passes, visited)
+            
+                    passes[shader.stem]['textures'][int(channel[-1])] = path[7:].split(".")[0]
+                else:
+                    logger.debug(f"Incompatible iChannel pre-processor: {shader.stem} {line}")
 
-    # parse source pass
-    for line in shader.read_text().splitlines():
-        if "fragCoord" in line:
-            line = line.replace("fragCoord", "gl_FragCoord")
+                valid_shader += f"\nuniform sampler2D {channel[1:]};\n"
 
-        if line.lstrip().startswith("#iChannel"):
-            # Parse out iChannels for texture uniforms binding.
-            logger.debug(f"Found iChannel in {shader.stem}: {line}")
+            elif line.lstrip().startswith("#include"):
+                # Include shader "headers" - GLSL has no #include directive.
 
-            channel, path = line.split(" ", 1)
-            path = path.strip('"')
-            if path.startswith("file://"):
-                if path[7:] not in visited:
-                    visited.add(path[7:])
-                    parse_image_passes(shader.parent / path[7:], passes, visited)
-          
-                passes[shader.stem]['textures'][int(channel[-1])] = path[7:].split(".")[0]
+                # TODO: Need to handle recursive includes - for now we're just assuming a common.glsl 
+                # include and no others - like Shadertoy.
+                logger.debug(f"Found include: {shader.stem} {line}")
+                include = shader.parent / line.split(" ", 1)[1].strip('"')
+                # TODO: Need to factor out parse_image_passes from actual shader parsing..
+                guard_str = lambda body: f"#ifndef {include.stem.upper()}_H\n#define {include.stem.upper()}_H\n{body}\n\n#endif\n"
+                valid_shader += guard_str(include.read_text())
+
+            elif line.lstrip().startswith("void mainImage("):
+                # Replace the mainImage function with the main function.
+                # This helps ensures well-formed source code for each pass and conforms to ShaderToy's API.
+                # TODO: Right now there is a hidden uv that can be used, but it's not obvious
+                #  - most shadertoy code goes fragCoord / iRes... We're also not using the defined IO in mainImage yet.
+                logger.debug(f"Found mainImage: {shader.stem} {line}")
+                valid_shader += "in vec2 uv;\nout vec4 fragColor;\n"
+                valid_shader += "void main()" + ("{\n" if "{" in line else "\n")
             else:
-                logger.debug(f"Incompatible iChannel pre-processor: {shader.stem} {line}")
+                # Just emit the line as-is.
+                valid_shader += f"{line}\n"
 
-            valid_shader += f"\nuniform sampler2D {channel[1:]};\n"
+        passes[shader.stem]['source'] = valid_shader
 
-        elif line.lstrip().startswith("#include"):
-            # Include shader "headers" - GLSL has no #include directive.
-
-            # TODO: Need to handle recursive includes - for now we're just assuming a common.glsl 
-            # include and no others - like Shadertoy.
-            logger.debug(f"Found include: {shader.stem} {line}")
-            include = shader.parent / line.split(" ", 1)[1].strip('"')
-            # TODO: Need to factor out parse_image_passes from actual shader parsing..
-            guard_str = lambda body: f"#ifndef {include.stem.upper()}_H\n#define {include.stem.upper()}_H\n{body}\n\n#endif\n"
-            valid_shader += guard_str(include.read_text())
-
-        elif line.lstrip().startswith("void mainImage("):
-            # Replace the mainImage function with the main function.
-            # This helps ensures well-formed source code for each pass and conforms to ShaderToy's API.
-            # TODO: Right now there is a hidden uv that can be used, but it's not obvious
-            #  - most shadertoy code goes fragCoord / iRes... We're also not using the defined IO in mainImage yet.
-            logger.debug(f"Found mainImage: {shader.stem} {line}")
-            valid_shader += "in vec2 uv;\nout vec4 fragColor;\n"
-            valid_shader += "void main()" + ("{\n" if "{" in line else "\n")
-        else:
-            # Just emit the line as-is.
-            valid_shader += f"{line}\n"
-
-    passes[shader.stem]['source'] = valid_shader
-
-    return passes
+        return passes
      
-
 
 def argparser():
     parser = argparse.ArgumentParser()
